7882c545b7d521d02d7d5d11f3810c4d4804c352
mohammedalfa <mohammedrafid2006@yahoo.com>
2023-05-30 16:24:22 -0400

    make game playable from console

--- script.js
+++ script.js
@@ -7,21 +7,35 @@ const gameBoard = (function() {
 
   const getBoard = () => _boardArray;
   const clearBoard = () => {
-    for (let i = 0; i < _boardArray.length; i++) {
+    for (let i = 0; i < 9; i++) {
       _boardArray[i] = "";
     }
   };
 
   const placeSymbol = (index, symbol) => {
-    if (_boardArray[index] !== "") return 0;
+    if (_boardArray[index] !== "") return 1;
     _boardArray[index] = symbol
-    return 1;
+    return 0;
+  };
+
+  const printBoard = () => {
+    let board = [];
+    let count = 0;
+    for (let i = 0; i < 3; i++) {
+      board[i] = [];
+      for (let j = 0; j < 3; j++) {
+        board[i][j] = _boardArray[count];
+        count++;
+      }
+    }
+    console.table(board);
   };
 
   return {
     getBoard,
-    placeSymbol,
     clearBoard,
+    placeSymbol,
+    printBoard,
   };
 })();
 
@@ -42,6 +56,7 @@ let player2 = newPlayer("2", "O");
 const gameController = (function(board, player1, player2) {
   let _ties = 0;
   let _currentPlayer = player1;
+  const _board = board.getBoard(); 
 
   const getPlayerOne = () => player1;
   const getPlayerTwo = () => player2;
@@ -52,13 +67,12 @@ const gameController = (function(board, player1, player2) {
   };
 
   const checkBoard = () => {
-    let cBoard = board.getBoard();
     let tie = true;
     let winner = false;
 
     // check for tie
-    for (let i = 0; i < cBoard.length; i++) {
-      if (cBoard[i] === "") {
+    for (let i = 0; i < 9; i++) {
+      if (_board[i] === "") {
         tie = false;
         break;
       }
@@ -69,18 +83,18 @@ const gameController = (function(board, player1, player2) {
     }
 
     // check rows
-    if (cBoard[0] === cBoard[1] && cBoard[0] === cBoard[2]) winner = cBoard[0];
-    if (cBoard[3] === cBoard[4] && cBoard[3] === cBoard[5]) winner = cBoard[3];
-    if (cBoard[6] === cBoard[7] && cBoard[6] === cBoard[8]) winner = cBoard[6];
+    if (_board[0] === _board[1] && _board[0] === _board[2]) winner = _board[0];
+    if (_board[3] === _board[4] && _board[3] === _board[5]) winner = _board[3];
+    if (_board[6] === _board[7] && _board[6] === _board[8]) winner = _board[6];
 
     // check columns
-    if (cBoard[0] === cBoard[3] && cBoard[0] === cBoard[6]) winner = cBoard[0];
-    if (cBoard[1] === cBoard[4] && cBoard[1] === cBoard[7]) winner = cBoard[1];
-    if (cBoard[2] === cBoard[5] && cBoard[2] === cBoard[8]) winner = cBoard[2];
+    if (_board[0] === _board[3] && _board[0] === _board[6]) winner = _board[0];
+    if (_board[1] === _board[4] && _board[1] === _board[7]) winner = _board[1];
+    if (_board[2] === _board[5] && _board[2] === _board[8]) winner = _board[2];
 
     // check diagonals
-    if (cBoard[0] === cBoard[4] && cBoard[0] === cBoard[8]) winner = cBoard[0];
-    if (cBoard[2] === cBoard[4] && cBoard[2] === cBoard[6]) winner = cBoard[2];
+    if (_board[0] === _board[4] && _board[0] === _board[8]) winner = _board[0];
+    if (_board[2] === _board[4] && _board[2] === _board[6]) winner = _board[2];
 
     if (winner === player1.getSymbol()) {
       player1.addWin();
@@ -91,6 +105,41 @@ const gameController = (function(board, player1, player2) {
     return winner;
   };
 
+  const printRound = () => {
+    board.printBoard();
+    console.log(`${_currentPlayer.getName()}'s turn`);
+  }
+
+  const playRound = (index) => {
+    if (board.placeSymbol(index, _currentPlayer.getSymbol()) === 1) {
+      console.log(`Cannot play at ${index}`);
+      return 0;
+    }
+    let status = checkBoard();
+    if (status === "tie") {
+      console.log("tie");
+      return 3;
+    } else if (status === player1.getSymbol()) {
+      console.log(`${player1.getName()} won`);
+      return 1;
+    } else if (status === player2.getSymbol()) {
+      console.log(`${player2.getName()} won`);
+      return 2;
+    }
+    switchCurrentPlayer();
+    printRound();
+    return 0;
+  };
+
+  const playGame = () => {
+    printRound();
+    let status;
+    do {
+      let move = prompt("Enter move: ");
+      status = playRound(move);
+    } while (status === 0);
+  }
+
   return {
     getPlayerOne,
     getPlayerTwo,
@@ -98,6 +147,9 @@ const gameController = (function(board, player1, player2) {
     getCurrentPlayer,
     switchCurrentPlayer,
     checkBoard,
+    printRound,
+    playRound,
+    playGame,
   };
 })(gameBoard, player1, player2);
 
@@ -107,4 +159,4 @@ const displayController = (function(game, board, doc) {
   const _gameGridDiv = doc.querySelector(".grid");
   const _gameScoreDiv = doc.querySelector(".game-score");
   
-})(document);
+})(gameController, gameBoard, document);
